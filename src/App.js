55e2bfb764f25be5c64cf04796af1e755fc98618
rcanai <canai3jp@gmail.com>
2018-09-09 16:55:40 +0900

    タスクのデータを管理して、書き換える処理を実装

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  // コンストラクターを記述
+  constructor() {
+    super();
+    this.state = {
+      // タスク用の配列データを設定
+      tasks: [
+        { title: 'タスク1', done: false },
+        { title: 'タスク2', done: true },
+        { title: 'タスク3', done: false },
+      ],
+    };
+  }
+
+  // タスクの状態を変更
+  changeDone (index) {
+    // 配列のコピーを作成して対象のインデックスのタスクを書き換える
+    const tasks = [...this.state.tasks];
+    tasks[index].done = !tasks[index].done;
+    this.setState({tasks: tasks});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>コードを変更</p>
+        <h1>タスク一覧</h1>
+        <div>
+          <span>完了:</span>
+          <output>1/3</output>
+        </div>
+        <ul className="tasks">
+          {this.state.tasks.map((task, index) => {
+            return <li key={index}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={task.done}
+                  onChange={() => this.changeDone(index)} />
+                <span>{task.title}</span>
+              </label>
+            </li>;
+          })}
+        </ul>
       </div>
     );
   }
